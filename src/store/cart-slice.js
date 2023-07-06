19a8282e0280ba53095d0db0fefe7d601f6b93fc
iurie.balan <iurie.balan@112.md>
2023-07-06 12:20:06 +0300

    optimized fetch data

--- src/store/cart-slice.js
+++ src/store/cart-slice.js
@@ -1,25 +1,30 @@
-import { createSlice } from "@reduxjs/toolkit";
-import { uiActions } from "./ui-slice";
+import { createSlice } from '@reduxjs/toolkit';
 
 const cartSlice = createSlice({
-  name: "cart",
+  name: 'cart',
   initialState: {
     items: [],
     totalQuantity: 0,
+    changed: false,
   },
   reducers: {
+    replaceCart(state, action) {
+      state.totalQuantity = action.payload.totalQuantity;
+      state.items = action.payload.items;
+    },
     addItemToCart(state, action) {
-      const newItem = action.payload; //we receive an item
-      const existingItem = state.items.find((item) => item.id === newItem.id); //we check if it already exists in items[] and if true, we just increase totalquantity
+      const newItem = action.payload;
+      const existingItem = state.items.find((item) => item.id === newItem.id);
       state.totalQuantity++;
+      state.changed = true;
       if (!existingItem) {
         state.items.push({
           id: newItem.id,
           price: newItem.price,
           quantity: 1,
           totalPrice: newItem.price,
-          title: newItem.title,
-        }); //thanks redux/toolkit we can write like these, as redux in background is doing the job
+          name: newItem.title,
+        });
       } else {
         existingItem.quantity++;
         existingItem.totalPrice = existingItem.totalPrice + newItem.price;
@@ -29,6 +34,7 @@ const cartSlice = createSlice({
       const id = action.payload;
       const existingItem = state.items.find((item) => item.id === id);
       state.totalQuantity--;
+      state.changed = true;
       if (existingItem.quantity === 1) {
         state.items = state.items.filter((item) => item.id !== id);
       } else {
@@ -39,51 +45,6 @@ const cartSlice = createSlice({
   },
 });
 
-export const sendCartData = (cart) => {
-  return async (dispatch) => {
-    dispatch(
-      uiActions.showNotification({
-        status: "pending",
-        title: "Sending...",
-        message: "Sending cart data!",
-      })
-    );
-
-    const sendRequest = async () => {
-      const response = await fetch(
-        "https://react-http-c5317-default-rtdb.europe-west1.firebasedatabase.app/cart.json",
-        {
-          method: "PUT", //put override existing data
-          body: JSON.stringify(cart),
-        }
-      );
-
-      if (!response.ok) {
-        throw new Error("Sending cart data failed");
-      }
-    };
-    try {
-      await sendRequest();
-      dispatch(
-        uiActions.showNotification({
-          status: "success",
-          title: "Success...",
-          message: "Sending cart data successfuly!",
-        })
-      );
-    } catch (error) {
-      sendCartData().catch((error) => {
-        dispatch(
-          uiActions.showNotification({
-            status: "error",
-            title: "Error...",
-            message: "Sending cart data failed!",
-          })
-        );
-      });
-    }
-  };
-};
-
 export const cartActions = cartSlice.actions;
+
 export default cartSlice;
--- src/store/ui-slice.js
+++ src/store/ui-slice.js
@@ -2,10 +2,7 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const uiSlice = createSlice({
   name: "ui",
-  initialState: {
-    cartIsVisible: false,
-    notification: null,
-  },
+  initialState: { cartIsVisible: false, notification: null },
   reducers: {
     toggle(state) {
       state.cartIsVisible = !state.cartIsVisible;
